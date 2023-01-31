ea5a233859d8d6938c2df307c1ed39876ec73955
t0ntin <115113636+t0ntin@users.noreply.github.com>
2023-01-30 22:47:17 -0500

    Several features added
    Added modal to get user input. Cards can now be added correctly to DOM.

--- js/script.js
+++ js/script.js
@@ -3,10 +3,53 @@ const authorNameField = document.querySelector("#author-name-field");
 const ratingField = document.querySelector("#rating-field");
 const readitCheckbox = document.querySelector("#readit");
 const createNewBookBtn = document.querySelector("#create-book-btn");
-const libraryContainer = document.querySelector(".library-container");
+const openModalBtn = document.querySelector('[data-modal-target]');
+const closeModalBtn = document.querySelector('[data-close-button]');
+const overlay = document.querySelector("#overlay");
 
+// const cardTitleHeading = document.createElement("h2");
+// const cardTitle = document.createElement("h3");
+// const cardAuthorHeading = document.createElement("h2");
+// const cardAuthor = document.createElement("h3");
+// const cardRatingHeading = document.createElement("h2");
+// const cardRating = document.createElement("h3");
+// const readitHeading = document.createElement("h3");
+
+
+openModalBtn.addEventListener("click", () =>{
+  const modal = document.querySelector(openModalBtn.dataset.modalTarget);
+  openModal(modal);
+});
+
+closeModalBtn.addEventListener("click", () =>{
+  const modal = closeModalBtn.closest('.modal');
+  closeModal(modal);
+});
+
+overlay.addEventListener("click", () => {
+  const modal = document.querySelector(openModalBtn.dataset.modalTarget);
+  closeModal(modal);
+})
+
+function openModal(modal) {
+  if(modal === null) {
+    return
+} else {
+  modal.classList.add('active');
+  overlay.classList.add('active');
+}
+};
+
+function closeModal(modal) {
+  if(modal === null) {
+    return
+} else {
+  modal.classList.remove('active');
+  overlay.classList.remove('active');
+}
+}
 let library = [];
-
+let libraryContainer;
 
 
 function Book (title, author, rating, readit) {
@@ -28,20 +71,86 @@ createNewBookBtn.addEventListener("click", (e) => {
   let author = authorNameField.value;
   let rating = ratingField.value;
   let readit = readitCheckbox.checked;
-  console.log(readit);
+  // console.log(readit);
   e.preventDefault();
 
   addBookToLibrary(title, author, rating, readit);
+  createBookEntry();
   console.log(library);
+  closeModal(modal);
 });
 
 function addBookToLibrary(title, author, rating, readit) {
-  console.log(readit);
+  // console.log(readit);
   const newBook = new Book (title, author, rating, readit);
   library.push(newBook);
   // console.log(newBook.logInfo());
 
 }
 
+function createBookEntry() {
+  if (!libraryContainer) {
+   libraryContainer = document.createElement('div');
+  document.body.append(libraryContainer);
+  libraryContainer.classList.add("library-container")
+  }
+
+  library.forEach((Book, index) => {
+
+    const existingCard = libraryContainer.querySelector(`[data-index="${index}"]`);
+    console.log(existingCard);
+    console.log(index);
+    console.log("existingCard = "+existingCard + ", index = " + index);
+    if (!existingCard) {
+    const cardContainer = document.createElement('div');
+    cardContainer.classList.add("card-container");
+    cardContainer.setAttribute("data-index", index);
+    libraryContainer.append(cardContainer);
+    const cardTitleHeading = document.createElement("h2");
+    const cardTitle = document.createElement("h3");
+    const cardAuthorHeading = document.createElement("h2");
+    const cardAuthor = document.createElement("h3");
+    const cardRatingHeading = document.createElement("h2");
+    const cardRating = document.createElement("h3");
+    const readitHeading = document.createElement("h3");
+    const deleteCardBtn = document.createElement("button");
+
+
+
+    cardContainer.append(cardTitleHeading);
+    cardTitleHeading.innerText = "Title";
+    cardContainer.append(cardTitle);
+    cardTitle.innerText = Book.title;
+
+    cardContainer.append(cardAuthorHeading);
+    cardAuthorHeading.innerText = "Author";
+    cardContainer.append(cardAuthor);
+    cardAuthor.innerText = Book.author;
+
+    cardContainer.append(cardRatingHeading);
+    cardRatingHeading.innerText = "Rating";
+    cardContainer.append(cardRating);
+    cardRating.innerText = Book.rating;
+
+    cardContainer.append(readitHeading);
+    readitHeading.innerText = "I have read it ✓✓"
+
+    cardContainer.append(deleteCardBtn);
+    deleteCardBtn.classList.add("delete-card-btn");
+    deleteCardBtn.innerText = "Delete";
+
+
+    }
+ 
+
+ 
+    
+ 
+
+  })
+ 
+}
+
 console.log(library);
+console.log();
 
